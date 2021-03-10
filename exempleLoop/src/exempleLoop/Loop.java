927093407c339237111dee5b4c65d8d09d7da8e5
AlbertProfe <albertprofe@gmail.com>
2021-03-10 18:46:45 +0100

    commit one, first commit

---
+++ exempleLoop/src/exempleLoop/Loop.java
@@ -0,0 +1,18 @@
+package exempleLoop;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		
+		//i have to do a commit to local repo
+		int number = 1;
+
+		while (number < 56) {
+			
+		    System.out.println("i love java so many times : "  +  number);
+		    number++;
+		}
+
+	}
+
+}
