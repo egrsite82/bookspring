2ac3b93458e6ac06d73cb44b44e0b10f3c6c0484
AlbertProfe <albertprofe@gmail.com>
2021-03-10 20:52:11 +0100

    commit thre, modify the program with boolean

--- exempleJava/src/exempleJava/Person.java
+++ exempleJava/src/exempleJava/Person.java
@@ -1,10 +1,26 @@
 package exempleJava;
 
+import java.util.Scanner;
+
 public class Person {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       //hello
+		
+		
+		Scanner inputdata = new Scanner(System.in);
+		
+		
+		System.out.println("Long live java");
+		
+		String tellmeyourname = inputdata.nextLine();
+		
+		boolean samename = tellmeyourname.equals("toni");
+		System.out.println(samename);
+		System.out.println("Your name is ... "+  tellmeyourname);
+		
+		
+		
+		
 	}
 
 }
