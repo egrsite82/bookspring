db04ce9ce20d9d9e045ce14612617bc9217898cc
User <User@DESKTOP-5D1LJ47>
2021-03-30 19:24:29 +0200

    commit five

--- Clinica3/src/clinica/MainApplication.java
+++ Clinica3/src/clinica/MainApplication.java
@@ -7,65 +7,81 @@ public class MainApplication {
 	
 	public static void main(String[] args) {
 		
+		//init, in this case, reader and handle events: while
 		System.out.println("loading ...."  + "\n");
 		Scanner reader = new Scanner(System.in);	
 			
 	    while (true) {
 	    	
-	    	//In MVR pattern design this lines are considered the View tier
-	    	//so we collect some data and show results to user
-	    	System.out.println("\nChoose one option ...");
-	    	System.out.println("0 - Quit");
-	    	System.out.println("1 - test Objects Person ");
-	    	System.out.println("2 - test Constructor Method Person");
-	    	System.out.println("3 - test Object Student");
-	    	System.out.println("4 - test Utilities Inherence Class Person");
-	    	System.out.println("5 - test Objects Course: Teacher/Student");
-	    	System.out.println("6 - test Clinica Members Array");
-	    	
-	    	System.out.println("\noption? ");
-	        String command = reader.nextLine();
-	        Utilities.clear();
-	
+	    	//our menu and I/0 system
+	    	String command = menuView(reader);
+	        	               
 	        
-	        //In MVR pattern design this lines are considered the Controller tier
-	    	//so we collect the requests at front controller and deliver them to
-	        //where they will be processed, in our case static methods at Test Class
-	        if (command.equals("0")) {
-	            break;
-	        } else if (command.equals("1")) {
-	        	Test.testObjectsPerson();
-	        } else if (command.equals("2")) {
-	        	Test.testConstructorMethod();
-	        } else if (command.equals("3")) {
-	        	Test.testObjectsStudent();
-	        } else if (command.equals("4")) {
-	        	Test.testUtilitiesInherence();
-	        } else if (command.equals("5")) {
-	        	Test.testObjectsCourse();
-	        } else if (command.equals("6")) {
-	        	Test.testClinicaMembersArray();
-	        } else {
-	        	System.out.println("Unknown command!");
-	        }
+	        int resultOperation = controller(command);
+	        
+	        if (resultOperation == 0) break;
+	        
 	    }
 	
-
-		 
-		//Test.testObjectsPerson();
-		//Test.testConstructorMethod();
-		//Test.testObjectsStudent();
-		//Test.testUtilitiesInherence();
-		//Test.testObjectsCourse();
-	    //Test.testClinicaMembersArray();
 	    
 		reader.close();
-	    
-		System.out.println("finishing ....\n" + "\n");
+	   	System.out.println("finishing ....\n" + "\n");
 
 	}
 	
+	//MVC >>> view
+	public static String menuView (Scanner reader) {
+		//In MVR pattern design this lines are considered the View tier
+    	//so we collect some data and show results to user
+    	System.out.println("\nChoose one option ...");
+    	System.out.println("0 - Quit");
+    	System.out.println("1 - test Objects Person ");
+    	System.out.println("2 - test Constructor Method Person");
+    	System.out.println("3 - test Object Student");
+    	System.out.println("4 - test Utilities Inherence Class Person");
+    	System.out.println("5 - test Objects Course: Teacher/Student");
+    	System.out.println("6 - test Clinica Members Array");
+    	
+    	System.out.println("\noption? ");
+        String command = reader.nextLine();
+        Utilities.clear();
+        
+        return command;
+		
+	}
 	
+	//MVC >>> controller
+	public static int controller (String command) {
+		
+		int resultOperation = -1;
+		
+		if (command.equals("0")) {
+			resultOperation = 0;
+		} else if (command.equals("1")) {
+			Test.testObjectsPerson();
+			resultOperation = 1;
+        } else if (command.equals("2")) {
+        	Test.testConstructorMethod();
+        	resultOperation = 2;
+        } else if (command.equals("3")) {
+        	Test.testObjectsStudent();
+        	resultOperation = 3;
+        } else if (command.equals("4")) {
+        	Test.testUtilitiesInherence();
+        	resultOperation = 4;
+        } else if (command.equals("5")) {
+        	Test.testObjectsCourse();
+        	resultOperation = 5;
+        } else if (command.equals("6")) {
+        	Test.testClinicaMembersArray();
+        	resultOperation = 6;
+        } else {
+        	System.out.println("Unknown command!");
+        	resultOperation = 7;
+        }
+		
+		return resultOperation;
+	}
 	
 
 }
