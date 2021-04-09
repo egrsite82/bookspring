34af2f44e9d0e09d28ee812e63eb2870a1beb33a
User <User@DESKTOP-5D1LJ47>
2021-04-09 20:52:40 +0200

    commit faker java with book

--- exemple1/src/main/java/com/example/exemple1/BookController.java
+++ exemple1/src/main/java/com/example/exemple1/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 
+
 @Controller
 @RequestMapping("/books")
 public class BookController {
@@ -15,23 +16,18 @@ public class BookController {
 @Autowired
 BookService	bookService;
 	
- 
-	@RequestMapping("/newBook")
-	public String createBook() {
-		
-		Book newbookAux = new Book();
-		
-		//call to service!!! >> newBookAux
-		return "newBookCreated";
-	}
-	
+ 	
 	
 	@RequestMapping("/allbooks")
-	public String showAllBooks(Model model) {
+	public String showAllBooks(Model containerToView) {
 		
-		model.addAttribute("booksfromController", bookService.queryBooks() );
+		containerToView.addAttribute("booksfromController", bookService.queryBooks() );
+		containerToView.addAttribute("fakebooksfromController",  bookService.queryFakeBooks());
 		
 		return "library";
 	}
+	
+	
+	
 
 }
--- exemple1/src/main/java/com/example/exemple1/BookService.java
+++ exemple1/src/main/java/com/example/exemple1/BookService.java
@@ -2,34 +2,55 @@ package com.example.exemple1;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
+
+import com.github.javafaker.Faker;
 
 @Service
 public class BookService {
-	
+
 	static List<Book> books = new ArrayList<Book>();
-	
-	
+	static List<Book> fakebooks = new ArrayList<Book>();
 	static {
-		
-		Book book1= new Book("Anna Karenina","BASGA15-252S38",568, 1872, "Leon Tolstoi");
-		Book book2= new Book("To The LightHouse","BSDGA15-278912",356, 1926, "Virginia Wolf");
-		Book book3= new Book("War and Peace","BAgyg15-250092",528, 1872, "Leon Tolstoi");
-		Book book4= new Book("Orlando","yuplA15-340112",356, 1926, "Virginia Wolf");
-		Book book5= new Book("Programming","RETEWRT-340112",356, 1926, "Engelbert");
+		Book book1 = new Book("Anna Karenina", "BASGA15-252S38", 568, 1872, "Leon Tolstoi");
+		Book book2 = new Book("To The LightHouse", "BSDGA15-278912", 356, 1926, "Virginia Wolf");
+		Book book3 = new Book("War and Peace", "BAgyg15-250092", 528, 1872, "Leon Tolstoi");
+		Book book4 = new Book("Orlando", "yuplA15-340112", 356, 1926, "Virginia Wolf");
+		Book book5 = new Book("Programming", "RETEWRT-340112", 356, 1926, "Engelbert");
 		books.add(book1);
 		books.add(book2);
 		books.add(book3);
 		books.add(book4);
 		books.add(book5);
 	}
-	
-	
-	public List<Book> queryBooks(){
-		
+
+	public List<Book> queryBooks() {
+
+		// there will be a REAL query
 		return books;
 	}
-	
-	
+
+	public List<Book> queryFakeBooks() {
+
+		
+
+		int i = 0;
+		Faker newfakebook = new Faker();
+		while (i < 10000) {
+
+			String author = newfakebook.book().author();
+			String genre = newfakebook.book().genre();
+			String title = newfakebook.book().title();
+
+			fakebooks.add(new Book(title, genre, getRandomNumber(100, 1250), getRandomNumber(1800, 2021), author));
+			i++;
+		}
+
+		return fakebooks;
+	}
+
+	public int getRandomNumber(int min, int max) {
+		return (int) ((Math.random() * (max - min)) + min);
+	}
+
 }
