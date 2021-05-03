830696dd06e9165d727f5df6c59ee5d15873aaf8
User <User@DESKTOP-5D1LJ47>
2021-05-03 20:40:13 +0200

    commit two

--- example2/src/main/java/com/example/example2/BookRestController.java
+++ example2/src/main/java/com/example/example2/BookRestController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequestMapping("/webapi")
 public class BookRestController {
 
 	@Autowired
@@ -18,7 +23,11 @@ public class BookRestController {
 
 	@PostMapping(path = "/addBook", consumes = "application/json")
 	public void insertBook(@RequestBody Book book) {
+		
+		
 		bookRepository.save(book);
+		
+		
 	}
 
 }
