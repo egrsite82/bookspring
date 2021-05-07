da4d65e355866a30d57eabaafb4545aafa5eb596
EGR <egrsite@gmail.com>
2021-05-07 08:25:13 +0200

    update v.- 1.0

--- example2/src/main/java/com/example/example2/BookRepository.java
+++ example2/src/main/java/com/example/example2/BookRepository.java
@@ -3,4 +3,5 @@ package com.example.example2;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book,String> {
+
 }
--- example2/src/main/java/com/example/example2/BookRestController.java
+++ example2/src/main/java/com/example/example2/BookRestController.java
@@ -1,8 +1,11 @@
 package com.example.example2;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,17 @@ public class BookRestController {
 		
 		
 	}
-
+	
+	@DeleteMapping("/delBook/{title}")
+	public void deleteBook(@PathVariable String title) 	{
+		
+		repository.deleteById(title);
+	}
+	
+	@PutMapping("/upBook/{title}")
+	public void updateBook(@PathVariable ("title") String title, @RequestBody Book book) 	{
+		
+		repository.deleteById(title);
+		repository.save(book);
+	}
 }
